28ff88e509059c6af119f8f3a45d61a3d0efcbb9
pirosgyb <pirosgyb@gmail.com>
2019-10-27 18:49:54 +0100

    Add animation to fragment navigation

--- app/src/main/java/com/bme/aut/parkingsearch/util/NavigationManager.kt
+++ app/src/main/java/com/bme/aut/parkingsearch/util/NavigationManager.kt
@@ -1,20 +1,42 @@
 package com.bme.aut.parkingsearch.util
 
 import androidx.navigation.NavController
+import androidx.navigation.NavOptions
 import com.bme.aut.parkingsearch.R
 
 object NavigationManager {
 
     fun navigateToAddParking(navController: NavController) {
-        navController.navigate(R.id.addParkingFragment)
+        val transitions = NavOptions.Builder()
+            .setEnterAnim(R.anim.enter_from_right)
+            .setExitAnim(R.anim.exit_to_left)
+            .setPopEnterAnim(R.anim.enter_from_left)
+            .setPopExitAnim(R.anim.exit_to_right)
+            .build()
+
+        navController.navigate(R.id.addParkingFragment, null, transitions)
     }
 
     fun navigateToDetails(navController: NavController) {
-        navController.navigate(R.id.parkingDetailsFragment)
+        val transitions = NavOptions.Builder()
+            .setEnterAnim(R.anim.enter_from_right)
+            .setExitAnim(R.anim.exit_to_left)
+            .setPopEnterAnim(R.anim.enter_from_left)
+            .setPopExitAnim(R.anim.exit_to_right)
+            .build()
+
+        navController.navigate(R.id.parkingDetailsFragment, null, transitions)
     }
 
     fun navigateToHome(navController: NavController) {
-        navController.navigate(R.id.homeFragment)
+        val transitions = NavOptions.Builder()
+            .setEnterAnim(R.anim.enter_from_left)
+            .setExitAnim(R.anim.exit_to_right)
+            .setPopEnterAnim(R.anim.enter_from_right)
+            .setPopExitAnim(R.anim.exit_to_left)
+            .build()
+
+        navController.navigate(R.id.homeFragment, null, transitions)
     }
 
 }
